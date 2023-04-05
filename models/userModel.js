56d525bf2c410c660914f9bdb876a202fa9854ba
Munir Abdullahi <abdullahimunir3@gmail.com>
2023-04-05 13:00:46 +0200

    feat: user model created

---
+++ models/userModel.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please tell us your first name"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please tell us your last name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please fill in your email address"],
+    validate: [validator.isEmail, "Please provide a valid email address"],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please fill in your password"],
+    minlength: 8,
+  },
+  phoneNumber: {
+    type: Number,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  },
+  amount: {
+    type: Number,
+    default: 0,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  accountNumber: {
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+  },
+  nextPaymentDate: {
+    type: Date,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  // Only hash the password if it has been modified or is new
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
+
+module.exports = mongoose.model("Users", userSchema);
